1bd84b6c0802518353f9e7aa81b1a61baad2df20
Ben <kwalijkje@live.com>
2024-05-08 12:47:19 +0200

    Update databaseDi

--- src/main/kotlin/io/memorix/Di.kt
+++ src/main/kotlin/io/memorix/Di.kt
@@ -1,14 +1,31 @@
 package io.memorix
 
-import org.jetbrains.exposed.sql.Database
+import io.memorix.database.DBConnector
+import io.memorix.database.DBConnectorFacade
+import org.koin.core.module.dsl.createdAtStart
+import org.koin.core.module.dsl.withOptions
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
+import java.lang.System.getProperty
 
 val applicationDi = module {
-    single<Database> {
-        Database.connect(
-            url = "jdbc:postgresql://" + getProperty("DB_HOST") + ":" + getProperty("DB_PORT") + "/" + getProperty("DB_NAME") + "?sslmode=disable",
-            user = getProperty("DB_USER"),
-            password = getProperty("DB_PASS")
+    val params = parametersOf(
+        getProperty("DB_HOST"),
+        getProperty("DB_PORT"),
+        getProperty("DB_NAME"),
+        getProperty("DB_USER"),
+        getProperty("DB_PASS"),
         )
+    println(params)
+    single<DBConnectorFacade> {
+        DBConnector(
+            dbHost = params.get<String>(),
+            dbPort = params.get<String>(),
+            dbName = params.get<String>(),
+            dbUser = params.get<String>(),
+            dbPass = params.get<String>()
+        )
+    } withOptions {
+        createdAtStart()
     }
 }
