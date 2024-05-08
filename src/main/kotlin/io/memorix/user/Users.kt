15c81ecff3ab07a5965054d9b0c2ea4233a60074
Ben <kwalijkje@live.com>
2024-05-08 12:46:44 +0200

    Users table definition

---
+++ src/main/kotlin/io/memorix/user/Users.kt
@@ -0,0 +1,12 @@
+package io.memorix.user
+
+import org.jetbrains.exposed.sql.Table
+
+object Users: Table() {
+    val id = uuid(name = "id")
+    val userName = varchar(name = "name", length = 128)
+    val userEmail = varchar(name = "email", length = 128)
+    val hashedPassword = varchar(name = "password", length = 128)
+
+    override val primaryKey = PrimaryKey(id)
+}
