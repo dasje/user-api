b2d4c4e1d95261ce5b3a120b8f34f921b9a08b08
Ben <kwalijkje@live.com>
2024-05-09 11:46:52 +0200

    Use statuspages to handle badrequest exceptions

--- src/main/kotlin/io/memorix/plugins/Routing.kt
+++ src/main/kotlin/io/memorix/plugins/Routing.kt
@@ -1,24 +1,30 @@
 package io.memorix.plugins
 
 import io.ktor.http.*
-import io.ktor.resources.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.resources.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.memorix.messages.ErrorTypes
+import io.memorix.messages.Errors
 import io.memorix.user.user
-import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     install(AutoHeadResponse)
     install(Resources)
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: ${cause}", status = HttpStatusCode.InternalServerError)
-            cause.printStackTrace()
+            when (cause) {
+                is BadRequestException ->
+                    call.respond(
+                        status = HttpStatusCode.BadRequest,
+                        message = Errors(error = ErrorTypes.UNPARSABLE_MESSAGE.errorDetail)
+                    )
+            }
+//            else -> call.respondText(text = "500: ${cause}", status = HttpStatusCode.InternalServerError)
         }
     }
     routing {
