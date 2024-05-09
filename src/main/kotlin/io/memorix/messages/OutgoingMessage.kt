bb714d2bd59806397f20d70d8e33fade452ba042
Ben <kwalijkje@live.com>
2024-05-09 14:51:32 +0200

    Update OutgoingMessage with SuccessUserResults

--- src/main/kotlin/io/memorix/messages/OutgoingMessage.kt
+++ src/main/kotlin/io/memorix/messages/OutgoingMessage.kt
@@ -7,6 +7,12 @@ import kotlinx.serialization.json.Json
 sealed class OutgoingMessage<out T> {
     @Serializable
     data class Success<out T>(val data: T) : OutgoingMessage<T>()
+
+    @Serializable
+    data class SuccessUserResults(val data: QueryUsersResponse) : OutgoingMessage<Nothing>(){
+        fun toJson(msg: QueryUsersResponse = this.data): String = Json.encodeToString(msg)
+    }
+
     @Serializable
     data class Error(val error: String) : OutgoingMessage<Nothing>() {
         fun toJson(msg: Error = this): String = Json.encodeToString(msg)
